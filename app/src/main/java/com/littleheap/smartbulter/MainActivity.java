bac8e1bc300116b84151b7da5bea381b39fc0acf
LittleHeap <418761605@qq.com>
2017-07-19 19:34:02 +0800

    update

--- app/src/main/java/com/littleheap/smartbulter/MainActivity.java
+++ app/src/main/java/com/littleheap/smartbulter/MainActivity.java
@@ -1,15 +1,79 @@
 package com.littleheap.smartbulter;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.littleheap.smartbulter.fragment.BulterFragment;
+import com.littleheap.smartbulter.fragment.PictureFragment;
+import com.littleheap.smartbulter.fragment.UserFragment;
+import com.littleheap.smartbulter.fragment.WechatFragment;
 import com.littleheap.smartbulter.ui.BaseActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+    private List<String> mTitle;
+    private List<Fragment> mFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //去阴影
+        getSupportActionBar().setElevation(0);
+        initData();
+        initView();
+    }
+
+    //初始化数据
+    private void initData(){
+        mTitle = new ArrayList<>();
+        mTitle.add("服务管家");
+        mTitle.add("微信精选");
+        mTitle.add("图片社区");
+        mTitle.add("个人中心");
+
+        mFragment = new ArrayList<>();
+        mFragment.add(new BulterFragment());
+        mFragment.add(new WechatFragment());
+        mFragment.add(new PictureFragment());
+        mFragment.add(new UserFragment());
+    }
+    //初始化View
+    private void initView(){
+        mTabLayout = (TabLayout) findViewById(R.id.mTabLayou);
+        mViewPager = (ViewPager) findViewById(R.id.mViewPager);
+
+        //预加载
+        mViewPager.setOffscreenPageLimit(mFragment.size());
+        //设置适配器
+        mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            //选中的item
+            @Override
+            public Fragment getItem(int position) {
+                return mFragment.get(position);
+            }
+            //返回item的个数
+            @Override
+            public int getCount() {
+                return mFragment.size();
+            }
+            //设置标题
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return mTitle.get(position);
+            }
+        });
+        //绑定
+        mTabLayout.setupWithViewPager(mViewPager);
     }
 }
---
+++ app/src/main/java/com/littleheap/smartbulter/fragment/BulterFragment.java
@@ -0,0 +1,23 @@
+package com.littleheap.smartbulter.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.littleheap.smartbulter.R;
+
+/**
+ * Created by Administrator on 2017/7/19 0019.
+ */
+
+public class BulterFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_bulter,null);
+        return view;
+    }
+}
